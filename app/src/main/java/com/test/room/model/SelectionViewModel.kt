048917b3688a0ed538c6b6b0ab8fac99e298c85d
ananth <ananth.kaniutrajasanthana@bnpparibasfortis.com>
2020-09-23 14:34:31 +0200

    Add viewModel for fragment

---
+++ app/src/main/java/com/test/room/model/SelectionViewModel.kt
@@ -0,0 +1,16 @@
+package com.test.room.model
+
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.test.room.R
+
+class SelectionViewModel : ViewModel() {
+    fun roomDatabaseSelected(findNavController: NavController) {
+        findNavController.navigate(R.id.action_selectionFragment_to_roomFragment)
+
+    }
+
+    fun apiSelected(findNavController: NavController) {
+        findNavController.navigate(R.id.action_selectionFragment_to_apiFragment)
+    }
+}
--- app/src/main/java/com/test/room/view/SelectionFragment.kt
+++ app/src/main/java/com/test/room/view/SelectionFragment.kt
@@ -1,7 +1,27 @@
 package com.test.room.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.test.room.R
+import com.test.room.model.SelectionViewModel
+import kotlinx.android.synthetic.main.fragment_selection.*
 
-class SelectionFragment: Fragment(R.layout.fragment_selection) {
+class SelectionFragment : Fragment(R.layout.fragment_selection) {
+
+    private val viewModel: SelectionViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button1.setOnClickListener {
+            viewModel.roomDatabaseSelected(findNavController())
+        }
+
+        button2.setOnClickListener {
+            viewModel.apiSelected(findNavController())
+        }
+    }
 }
