7b0459b40664ecacf01325c49107e8ee7e1a83d3
ananth <ananth.kaniutrajasanthana@bnpparibasfortis.com>
2020-09-23 21:37:36 +0200

    NukeTable when viewmodel loaded

--- app/src/main/java/com/test/room/viewmodel/SelectionViewModel.kt
+++ app/src/main/java/com/test/room/viewmodel/SelectionViewModel.kt
@@ -1,18 +1,24 @@
 package com.test.room.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.test.room.R
 import com.test.room.db.AppDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class SelectionViewModel(db: AppDatabase) : ViewModel() {
+class SelectionViewModel(private val db: AppDatabase) : ViewModel() {
     init {
-        db.userDao().nukeTable()
+        viewModelScope.launch(IO) {
+            db.userDao().nukeTable()
+        }
     }
 
     fun roomDatabaseSelected(findNavController: NavController) {
         findNavController.navigate(R.id.action_selectionFragment_to_roomFragment)
-
     }
 
     fun apiSelected(findNavController: NavController) {
